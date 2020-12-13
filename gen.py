7ddeeb5c7ce2937e98ca2b4415956010ab9ef186
Alexander Golovanov <Golovanov12345@gmail.com>
2020-12-13 13:47:51 +0300

    Add continued fractions

--- gen.py
+++ gen.py
@@ -5,4 +5,6 @@ import random
 t = 100
 print(t)
 for i in range(t):
-	print(random.randint(1, 10 ** 18))
+	n, m = random.randint(1, 10**9), random.randint(1, 10**9)
+	a, b = random.randint(0, m - 1), random.randint(0, m - 1)
+	print(n, m, a, b)
