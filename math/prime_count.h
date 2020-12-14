e85f49373ae9373391d0303dd3d0bfa037d8ece5
Alexander Golovanov <Golovanov12345@gmail.com>
2020-12-15 02:38:16 +0300

    Fix prime count a little and add fill_dp function

--- math/prime_count.h
+++ math/prime_count.h
@@ -14,23 +14,33 @@ public:
 	vector<int> erat, primes;
 	vector<int> sorted_numbers;
 	vector<vector<li>> dp;
+	vector<li> prec;
 
-	explicit PrimeCount(li _n): n(_n), K(max(10., pow(n / max(1., log(n)), 2. / 3))) {
+	explicit PrimeCount(li _n): n(_n), K(max({20., pow(n / max(1., log(n)), 2. / 3), sqrt(n) + 100})) {
 		tie(erat, primes) = sieve(K);
 		dp.resize(n / (K - 1) + 1);
-		int sz = 0;
 		for (int i = 1, j = (int)primes.size() - 1; n / i >= K; ++i) {
 			while (j > 0 && sqr(primes[j - 1]) > n / i) {
 				--j;
 			}
 			dp[i].assign(j + 2, def);
-			sz += dp[i].size();
+		}
+		while (!dp.empty() && dp.back().empty()) {
+			dp.pop_back();
 		}
 		sorted_numbers.resize(K - 1);
 		iota(all(sorted_numbers), 1);
 		sort(all(sorted_numbers), [&](int i, int j) {
 			return erat[i] > erat[j];
 		});
+
+		prec.resize(K);
+		for (int p : primes) {
+			prec[p] += 1;	// here
+		}
+		for (int i = 1; i < (int)prec.size(); ++i) {
+			prec[i] += prec[i - 1];
+		}
 	}
 
 	struct Query {
@@ -40,32 +50,75 @@ public:
 		li ans;
 	};
 
+	void fill_dp() {
+		if (dp.empty()) {
+			return;
+		}
+		Fenwick<li> f(K);
+		for (int i = 1; i < K; ++i) {
+			f.add(i, 1);	// here
+		}
+		for (auto& v : dp) {
+			fill(all(v), 0);
+		}
+		int idx = (int)sorted_numbers.size() - 2;
+		for (int j = 0; j < (int)dp[1].size(); ++j) {
+			for (int i = 1; i < (int)dp.size() && j < (int)dp[i].size(); ++i) {
+				if (j > 0 && n / i >= K && i * primes[j - 1] >= (int)dp.size()) {
+					dp[i][j] -= f.get(n / i / primes[j - 1]);
+				}
+			}
+			while (idx >= 0 && erat[sorted_numbers[idx]] < primes[j]) {
+				f.add(sorted_numbers[idx], -1);	// here
+				--idx;
+			}
+			for (int i = 1; i < (int)dp.size() && j < (int)dp[i].size(); ++i) {
+				if (j == 0) {
+					dp[i][j] += n / i;	// here
+				} else if (n / i < K) {
+					dp[i][j] += f.get(n / i);
+				} else {
+					dp[i][j] += dp[i][j - 1];
+					if (li ni = i * primes[j - 1]; ni < (int)dp.size()) {
+						int mn = min(j - 1, (int)dp[ni].size() - 1);
+						dp[i][j] -= dp[ni][mn] + mn - (j - 1);
+					}
+				}
+			}
+		}
+	}
+
 	li calc(int i) {
+		if (n / i < (int)prec.size()) {
+			return prec[n / i];
+		}
 		vector<Query> qrs;
 		fill_queries(i, (int)dp[i].size(), qrs);
-		sort(all(qrs), [&](const Query& a, const Query& b) {
-			return a.k > b.k;
-		});
-		Fenwick<li> f(K);
-		f.add(1, 1);	// here
-		int idx = 0;
-		for (int k : sorted_numbers) {
-			if (idx >= (int)qrs.size()) {
-				break;
+		if (!qrs.empty()) {
+			sort(all(qrs), [&](const Query& a, const Query& b) {
+				return a.k > b.k;
+			});
+			Fenwick<li> f(K);
+			f.add(1, 1);	// here
+			int idx = 0;
+			for (int k : sorted_numbers) {
+				if (idx >= (int)qrs.size()) {
+					break;
+				}
+				while (idx < (int)qrs.size() && erat[k] < primes[qrs[idx].k]) {
+					qrs[idx].ans = f.get(qrs[idx].n);
+					++idx;
+				}
+				f.add(k, 1);	// here
 			}
-			while (idx < (int)qrs.size() && erat[k] < primes[qrs[idx].k]) {
+			while (idx < (int)qrs.size()) {
 				qrs[idx].ans = f.get(qrs[idx].n);
 				++idx;
 			}
-			f.add(k, 1);	// here
+			sort(all(qrs), [&](const Query& a, const Query& b) {
+				return a.id < b.id;
+			});
 		}
-		while (idx < (int)qrs.size()) {
-			qrs[idx].ans = f.get(qrs[idx].n);
-			++idx;
-		}
-		sort(all(qrs), [&](const Query& a, const Query& b) {
-			return a.id < b.id;
-		});
 		return calc_queries(i, (int)dp[i].size(), qrs) + (int)dp[i].size() - 1;	// here
 	}
 
@@ -95,7 +148,6 @@ private:
 		} else if (curn < K) {
 			li ans = qrs.back().ans;
 			qrs.pop_back();
-			// cerr << "there are " << ans << " numbers <= " << curn << " with min prime >= " << primes[j] << "\n";
 			return ans;
 		} else if (j >= (int)dp[i].size()) {
 			return calc_queries(i, (int)dp[i].size() - 1, qrs) + ((int)dp[i].size() - 1) - j;
@@ -105,7 +157,6 @@ private:
 			li ans = 0;
 			ans += calc_queries(i, j - 1, qrs);
 			ans -= calc_queries(i * primes[j - 1], j - 1, qrs);
-			// cerr << "there are " << ans << " numbers <= " << curn << " with min prime >= " << primes[j] << "\n";
 			return dp[i][j] = ans;
 		}
 	}
