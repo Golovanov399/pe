d7a23031e9b695b386ca6a5c3fa5ea89041b7197
Alexander Golovanov <Golovanov12345@gmail.com>
2021-01-29 23:58:16 +0300

    Remove a comment

--- base/base.h
+++ base/base.h
@@ -5,6 +5,5 @@
 #define all(x) (x).begin(), (x).end()
 #define make_unique(x) sort((x).begin(), (x).end()); (x).erase(unique((x).begin(), (x).end()), (x).end())
 #define imie(x) #x << ": " << x
-// #define repeat(n) for (int __macro_repeat_##__COUNTER__ = 0; __macro_repeat_##__COUNTER__ < n; ++__macro_repeat_##__COUNTER__)
 
 using namespace std;
