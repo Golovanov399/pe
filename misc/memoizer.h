d9277bd9545fdde5c8c7c1f5fcb2d59e04531e89
Alexander Golovanov <Golovanov12345@gmail.com>
2021-04-30 18:23:46 +0300

    Add memoizers

---
+++ misc/memoizer.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "../base/base.h"
+
+template <typename S, typename T, typename Container, typename Fun>
+class GenericMemoizerResult {
+public:
+	template <typename F>
+	explicit GenericMemoizerResult(F&& _f): f(forward<F>(_f)), cache({}) {}
+
+	T operator ()(const S& x) {
+		if constexpr (is_same_v<Container, map<S, T>> || is_same_v<Container, unordered_map<S, T>>) {
+			if (auto it = cache.find(x); it != cache.end()) {
+				return it->second;
+			}
+		} else {
+			// TODO: replace by requiring find()
+			if (cache.count(x)) {
+				return cache[x];
+			}
+		}
+		auto res = f(ref(*this), x);
+		return cache[x] = res;
+	}
+
+private:
+	Fun f;
+	Container cache;
+};
+
+template <typename S, typename T, typename Container = map<S, T>, typename Fun = void>
+decltype(auto) GenericMemoizer(Fun&& fun) {
+	return GenericMemoizerResult<S, T, Container, decay_t<Fun>>(forward<Fun>(fun));
+}
+
+
+template <typename S, typename T, typename Container, typename Fun>
+class MemoizerResult {
+public:
+	template <typename F>
+	explicit MemoizerResult(F&& _f, size_t size = 1000000): f(forward<F>(_f)), small_cache(size), calculated(size, false), cache({}) {}
+
+	static_assert(is_integral_v<S>);
+
+	T operator ()(S x) {
+		if (x < small_cache.size()) {
+			if (calculated[x]) {
+				return small_cache[x];
+			}
+		} else if constexpr (is_same_v<Container, map<S, T>> || is_same_v<Container, unordered_map<S, T>>) {
+			if (auto it = cache.find(x); it != cache.end()) {
+				return it->second;
+			}
+		} else {
+			// TODO: replace by requiring find()
+			if (cache.count(x)) {
+				return cache[x];
+			}
+		}
+		auto res = f(ref(*this), x);
+		if (x < small_cache.size()) {
+			calculated[x] = true;
+			small_cache[x] = res;
+			return res;
+		} else {
+			return cache[x] = res;
+		}
+	}
+
+private:
+	Fun f;
+	vector<T> small_cache;
+	vector<bool> calculated;
+	Container cache;
+};
+
+template <typename S, typename T, typename Container = map<S, T>, typename Fun = void>
+decltype(auto) Memoizer(Fun&& fun, size_t size = 1000000) {
+	return MemoizerResult<S, T, Container, decay_t<Fun>>(forward<Fun>(fun), size);
+}
