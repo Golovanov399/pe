0e0af8f286f9975e8baabfa9ac401b32fa3f26e5
Alexander Golovanov <Golovanov12345@gmail.com>
2021-05-01 01:53:58 +0300

    Add hasher

---
+++ strings/hash.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../base/base.h"
+
+template <int N>
+struct Hasher {
+	using Hash = array<int, N>;
+	array<int, N> mods;
+	vector<Hash> pwrs;
+	vector<Hash> h;
+
+	Hasher(const array<pair<int, int>, N>& bases, const string& s) {
+		const int n = s.size();
+		for (int i = 0; i < N; ++i) {
+			mods[i] = bases[i].first;
+		}
+		pwrs.resize(n + 1);
+		h.assign(n + 1, {});
+		pwrs[0].fill(1);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < N; ++j) {
+				pwrs[i + 1][j] = (1ll * pwrs[i][j] * bases[j].second) % mods[j];
+				h[i + 1][j] = (1ll * h[i][j] * bases[j].second + s[i]) % mods[j];
+			}
+		}
+	}
+
+	Hash get(int l, int r) const {
+		Hash res = h[r];
+		for (int i = 0; i < N; ++i) {
+			res[i] = (res[i] - 1ll * h[l][i] * pwrs[r - l][i] % mods[i] + mods[i]) % mods[i];
+		}
+		return res;
+	}
+};
