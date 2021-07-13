1386400aa6d60e1a95fd8c471082d72c3fe6e50f
Alexander Golovanov <Golovanov12345@gmail.com>
2021-07-13 16:01:22 +0300

    Add std:: to indexed_value

--- base/indexed_value.h
+++ base/indexed_value.h
@@ -6,18 +6,18 @@ struct IndexedValue {
 	T value;
 
 	bool operator <(const IndexedValue<T>& ot) const {
-		return tie(value, index) < tie(ot.value, ot.index);
+		return std::tie(value, index) < std::tie(ot.value, ot.index);
 	}
 };
 
 template <typename T>
-ostream& operator <<(ostream& ostr, const IndexedValue<T>& x) {
+std::ostream& operator <<(std::ostream& ostr, const IndexedValue<T>& x) {
 	return ostr << "(index=" << x.index << ", value=" << x.value << ")";
 }
 
 template <typename T>
-vector<IndexedValue<T>> with_index(const vector<T>& vec) {
-	vector<IndexedValue<T>> result(vec.size());
+std::vector<IndexedValue<T>> with_index(const std::vector<T>& vec) {
+	std::vector<IndexedValue<T>> result(vec.size());
 	for (int i = 0; i < (int)vec.size(); ++i) {
 		result[i] = {i, vec[i]};
 	}
