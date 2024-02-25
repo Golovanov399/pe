da25729b3acaf2f341ef2b7ac569d4a54b4d8d4b
Alexander Golovanov <Golovanov12345@gmail.com>
2024-02-25 19:20:46 +0300

    Fix next_size so that it works on codeforces

--- base/traits.h
+++ base/traits.h
@@ -9,12 +9,11 @@ using ld = long double;
 using uint = unsigned int;
 using ull = unsigned long long;
 
-template <typename> struct next_size;
+template <typename, typename D = void> struct next_size;
 template <typename T> using next_size_t = typename next_size<T>::type;
 template <typename T> struct tag { using type = T; };
 
 template <> struct next_size<int8_t> : tag<int16_t> {};
 template <> struct next_size<int16_t> : tag<int32_t> {};
 template <> struct next_size<int32_t> : tag<int64_t> {};
-template <> struct next_size<int64_t> : tag<__int128_t> {};
-template <> struct next_size<long long> : tag<__int128_t> {};
+template <typename T> struct next_size<T, std::enable_if_t<4 < sizeof(T) && sizeof(T) <= 8, void>> : tag<__int128_t> {};
